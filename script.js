0b29ba0f971386ce62b375a4d1854956574593b1
hamidvolkan <hamidvolkan.3m260@yahoo.com>
2024-09-17 22:20:32 +0330

    last commit front-end

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const burger = document.querySelectorAll(".order__Card");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+    } else entry.target.classList.remove("animate");
+  });
+});
+
+burger.forEach((burger) => {
+  observer.observe(burger);
+});
+
+// make animation for last part image
+const eventImage = document.querySelectorAll(".event__image");
+const eventContent = document.querySelectorAll(".event__content");
+
+const observing = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      console.log("happening");
+    } else entry.target.classList.remove("animate");
+  });
+});
+
+eventImage.forEach((item) => {
+  observing.observe(item);
+});
+eventContent.forEach((item) => {
+  observing.observe(item);
+});
